973c27b188d0871ee466bef03d91a895da4a9115
Molid <delmolid@hotmail.fr>
2025-04-22 00:48:51 +0200

    authentification avec les utilisateur de la base de données reussie

--- src/main/java/com/back/hello_ada_back/SecurityConfig.java
+++ src/main/java/com/back/hello_ada_back/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.back.hello_ada_back;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -10,6 +11,14 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
+
+
+import com.back.hello_ada_back.services.CustomUserDetailsService;
 
 
 
@@ -17,32 +26,64 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 @EnableWebSecurity
 public class SecurityConfig {
 
+@Autowired 
+private CustomUserDetailsService customUserDetailsService;
 
+
+
+@Bean 
+public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+    AuthenticationManagerBuilder authenticationManagerBuilder = http
+    .getSharedObject(AuthenticationManagerBuilder.class);
+    authenticationManagerBuilder
+        .userDetailsService(customUserDetailsService)
+        .passwordEncoder(passwordEncoder());  // Utilisation du PasswordEncoder
+    return authenticationManagerBuilder.build();
+}
 
     @Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
-			.authorizeHttpRequests((requests) -> requests
-				.anyRequest().authenticated()
+			.csrf(csrf -> csrf.disable())
+			.headers(headers -> headers
+				.frameOptions()
+				.sameOrigin()
 			)
-			.formLogin((form) -> form
+			.authorizeHttpRequests(auth -> {
+                auth.requestMatchers("/h2-console/**").permitAll();
+                auth.requestMatchers("/api/users/createUser").permitAll(); // Permettre la création d'utilisateur
+				auth.requestMatchers("/login").permitAll();
+				auth.anyRequest().authenticated();
+			})
+			.formLogin(form -> form
 				.defaultSuccessUrl("/api/posts", true)
 				.permitAll()
 			)
-			.logout((logout) -> logout.permitAll());
+			.logout(logout -> logout
+				.logoutUrl("/api/logout")  
+				.logoutSuccessUrl("/login")  
+				.invalidateHttpSession(true)  
+				.deleteCookies("JSESSIONID")  
+				.permitAll()
+			);
 
 		return http.build();
 	}
 
-    @Bean
-	public UserDetailsService userDetailsService() {
-		UserDetails user =
-			 User.withDefaultPasswordEncoder()
-				.username("user")
-				.password("password")
-				.roles("USER")
-				.build();
+    // @Bean
+	// public UserDetailsService userDetailsService() {
+	// 	UserDetails user =
+	// 		 User.withDefaultPasswordEncoder()
+	// 			.username("user")
+	// 			.password("password")
+	// 			.roles("USER")
+	// 			.build();
 
-		return new InMemoryUserDetailsManager(user);
+	// 	return new InMemoryUserDetailsManager(user);
+	// }
+
+    @Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();  // Remplace NoOpPasswordEncoder
 	}
 }
